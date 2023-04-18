0e5d54fe1c676ad360822c4c10ce263a2119b994
matee-safi <matiullahsafibbc@gmail.com>
2023-04-19 00:34:07 +0430

    Fetch missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,3 +1,38 @@
-const Missions = () => <div>Missions</div>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missionsSlice';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
+
+  return (
+    <div>
+      <h1>Missions</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.mission_id.map((mission, index) => (
+            <tr key={mission}>
+              <th>{missions.mission_name[index]}</th>
+              <td>{missions.description[index]}</td>
+              <td>Not a member</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,10 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {};
+const initialState = {
+  mission_id: [],
+  mission_name: [],
+  description: [],
+};
+
+export const getMissions = createAsyncThunk('missions/getMissions', async () => {
+  try {
+    const response = await axios('https://api.spacexdata.com/v3/missions');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+});
 
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getMissions.fulfilled, (state, action) => {
+      action.payload.forEach(() => {
+        state.mission_id = action.payload.map((mission) => mission.mission_id);
+        state.mission_name = action.payload.map((mission) => mission.mission_name);
+        state.description = action.payload.map((mission) => mission.description);
+      });
+    });
+  },
 });
 
 export default missionsSlice.reducer;
