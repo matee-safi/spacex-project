98513293cc4fa35508fc49b8883012195e8ab7e6
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-04-18 14:48:50 -0600

    fetch data from the SpaceX API

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,3 +1,28 @@
-const Rockets = () => <div>Rockets</div>;
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+
+const Rockets = () => {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <main>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {rockets.map((rocket) => (
+        <article key={rocket.id}>
+          <h1>{rocket.name}</h1>
+          <img src={rocket.flickr_images[0]} alt={rocket.name} />
+          <p>{rocket.description}</p>
+        </article>
+      ))}
+    </main>
+  );
+};
 
 export default Rockets;
