bfb58ccf5af1ff93c7bd245b8b8b82aee9669423
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-04-18 11:22:14 -0600

    Add routes and view

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,20 @@
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', label: 'Rockets' },
+  { path: 'missions', label: 'Missions' },
+  { path: 'my-profile', label: 'My Profile' },
+];
+
 const Nav = () => (
   <nav>
-    nav
+    <ul>
+      {links.map(({ path, label }) => (
+        <li key={path}>
+          <NavLink to={path}>{label}</NavLink>
+        </li>
+      ))}
+    </ul>
   </nav>
 );
 
