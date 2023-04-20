2914f499e6a32b44d7bce16f7a426ce1779a2a27
matee-safi <matiullahsafibbc@gmail.com>
2023-04-21 02:49:52 +0430

    Add joined missions to my profile

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -1,7 +1,20 @@
-const JoinedMissions = () => (
-  <>
-    <h1 className="sectionTitle">My Missions</h1>
-  </>
-);
+import { useSelector } from 'react-redux';
+import '../styles/JoinedMissions.css';
+
+const JoinedMissions = () => {
+  const missions = useSelector((state) => state.missions);
+  return (
+    <>
+      <h1>My Missions</h1>
+      {missions.missions
+        .filter((mission) => mission.reserved)
+        .map((mission) => (
+          <article key={mission.mission_id} className="joined-missions">
+            <p>{mission.mission_name}</p>
+          </article>
+        ))}
+    </>
+  );
+};
 
 export default JoinedMissions;
