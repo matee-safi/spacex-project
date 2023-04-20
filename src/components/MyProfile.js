52aaf0cb17c35eea4f629ed5144b7649e5bb2d6b
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-04-19 18:40:39 -0600

    My Profile setup

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,3 +1,16 @@
-const MyProfile = () => <div>MyProfile</div>;
+import JoinedMissions from './JoinedMissions';
+import ReservedRockets from './ReservedRockets';
+import '../styles/MyProfile.css';
+
+const MyProfile = () => (
+  <main className="myProfileMain">
+    <section className="missionsSection">
+      <JoinedMissions />
+    </section>
+    <section className="rocketsSection">
+      <ReservedRockets />
+    </section>
+  </main>
+);
 
 export default MyProfile;
---
+++ src/components/ReservedRockets.js
@@ -0,0 +1,7 @@
+const ReservedRockets = () => (
+  <>
+    <h1 className="sectionTitle">My Rockets</h1>
+  </>
+);
+
+export default ReservedRockets;
