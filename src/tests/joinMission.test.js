d942bf833420c0e8ca2df5bf512e34ff74143ef7
matee-safi <matiullahsafibbc@gmail.com>
2023-04-21 06:14:55 +0430

    add tests to missions

---
+++ src/tests/joinMission.test.js
@@ -0,0 +1,13 @@
+import { joinMission } from '../redux/missions/missionsSlice';
+
+describe('joinMission', () => {
+    test('should return the correct action', () => {
+      const id = '1';
+      const expectedAction = {
+        type: 'missions/joinMission',
+        payload: id,
+      };
+      expect(joinMission(id)).toEqual(expectedAction);
+    });
+  });
+  
