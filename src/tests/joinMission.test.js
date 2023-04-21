b0392b8e72d5e414290f9014f0818ac23663de17
matee-safi <matiullahsafibbc@gmail.com>
2023-04-21 06:17:50 +0430

    Fix linter errors

--- src/tests/joinMission.test.js
+++ src/tests/joinMission.test.js
@@ -1,13 +1,12 @@
 import { joinMission } from '../redux/missions/missionsSlice';
 
 describe('joinMission', () => {
-    test('should return the correct action', () => {
-      const id = '1';
-      const expectedAction = {
-        type: 'missions/joinMission',
-        payload: id,
-      };
-      expect(joinMission(id)).toEqual(expectedAction);
-    });
+  test('should return the correct action', () => {
+    const id = '1';
+    const expectedAction = {
+      type: 'missions/joinMission',
+      payload: id,
+    };
+    expect(joinMission(id)).toEqual(expectedAction);
   });
-  
+});
